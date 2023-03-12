dc519925da62ec17a45f6d843a22520b06ec0f7c
DaviFeitosa21 <davisfeitosa21@gmail.com>
2023-03-12 17:37:56 -0300

    Initial commit

---
+++ Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2Poo3
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double n1, n2, n3;
+
+        public double MediaAluno()
+        {
+            return n1 + n2 + n3;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex2Poo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno p1 = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            p1.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            Console.Write("Nota 1: ");
+            p1.n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 2: ");
+            p1.n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 3: ");
+            p1.n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine("Nota final: " + p1.MediaAluno().ToString("F2", CultureInfo.InvariantCulture));
+
+            if(p1.MediaAluno() >= 60)
+            {
+                Console.WriteLine("APROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO!");
+                double notaRestante = (60 - p1.MediaAluno());
+                Console.WriteLine("Faltaram: " + notaRestante.ToString("F2", CultureInfo.InvariantCulture) + " Pontos ");
+            }
+
+        }
+    }
+}
